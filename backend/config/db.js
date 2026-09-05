cc5ca9a619b67e2333edc1a0141ca20efd03d6f6
Turaab Haider <turaab.haider0110@gmail.com>
2026-09-05 10:50:12 -0700

    Improve database connection diagnostics

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
